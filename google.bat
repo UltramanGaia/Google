e37cd60adb941488681e59ac77a21befffd94b69
UltramanGaia <3213359017@qq.com>
2016-04-29 15:11:46 +0800

    ok

--- google.bat
+++ google.bat
@@ -1,4 +1,4 @@
-@echo off
+锘緻echo off
 color 0A 
 echo                                                                          ***                
 echo                                                                          ***                
@@ -28,7 +28,7 @@ echo                                                                  ***
 echo                                                                  ***                        
 echo                                                         **     ****                         
 echo                                                         **********                          
-echo ~~请右键以管理员身份运行~~
+echo ~~璇峰彸閿互绠＄悊鍛樿韩浠借繍琛寏~
 echo                                                       ------------------by UltramanGaia
 cd /d %~dp0
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O C:\Windows\System32\drivers\etc\hosts  -q https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts
