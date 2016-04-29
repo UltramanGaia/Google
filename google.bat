b717efd732b45b5b1f6cb0df8783b7a4f61e6d66
UltramanGaia <3213359017@qq.com>
2016-04-29 16:10:35 +0800

    interesting

--- google.bat
+++ google.bat
@@ -32,5 +32,4 @@ echo ~~请右键以管理员身份运行~~
 echo                                                       ------------------by UltramanGaia
 cd /d %~dp0
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O C:\Windows\System32\drivers\etc\hosts  -q https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts
-echo OK
 pause
