f48b539954370c43836402232532a9f17714971d
UltramanGaia <3213359017@qq.com>
2017-10-13 15:03:46 +0800

    change

--- google.bat
+++ google.bat
@@ -1,36 +1,99 @@
 @echo off
+
 color 0A 
+
+
 echo                                                                          ***                
+
+
 echo                                                                          ***                
+
+
 echo         *******                                                          ***                
+
+
 echo       ***********                                                        ***                
+
 echo      ****      ***                                                       ***                
+
+
 echo     ***          *                                                       ***                
+
+
 echo    ***                                                                   ***                
+
+
 echo   ***                     ******           ******          *****  ***    ***       ******   
+
+
 echo   ***                   **********       **********      ******** ***    ***      ********  
+
+
 echo  ***                   ***     ****     ***     ****     ***    *****    ***     ***    *** 
+
+
 echo  ***                  ***       ***    ***       ***    ***      ****    ***    ***     *** 
+
+
 echo  ***       *******    ***       ****   ***       ****   ***       ***    ***    **       ***
+
+
 echo  ***       *******   ***         ***  ***         ***  ***        ***    ***   ***       ***
+
+
 echo  ***           ***   ***         ***  ***         ***  ***        ***    ***   *************
+
+
 echo  ***           ***   ***         ***  ***         ***  ***        ***    ***   *************
+
+
 echo  ****          ***   ***         ***  ***         ***  ***        ***    ***   ***          
 echo   ***          ***   ***         ***  ***         ***  ***        ***    ***   ***          
+
+
 echo   ****         ***   ****        **   ****        **   ***        ***    ***   ***          
+
+
 echo    ****        ***    ***       ***    ***       ***    ***      ****    ***    ***       * 
+
+
 echo     ****      ****     ***     ***      ***     ***     ****    *****    ***    ****     ** 
+
+
 echo      *************      *********        *********       ******** ***    ***     *********  
+
+
 echo        ********          ******           ******           *****  ***    ***       ******   
+
+
 echo                                                                   ***                       
+
+
 echo                                                                   ***                       
+
+
 echo                                                                  ***                        
+
+
 echo                                                                  ***                        
+
+
 echo                                                         **     ****                         
+
+
 echo                                                         **********                          
+
+
 echo ~~请右键以管理员身份运行~~
+
+
 echo                                                       ------------------by UltramanGaia
+
+
 cd /d %~dp0
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O C:\Windows\System32\drivers\etc\hosts  -q https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts
+
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O C:\Windows\System32\drivers\etc\hosts  -q https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts-files/hosts
+
 start  https://www.google.com.hk/
+
 pause
